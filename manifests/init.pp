0fb98df00f635ad336f36574eda16ad24479bd31
Bruno Léon <bruno.leon@savoirfairelinux.com>
2012-01-05 15:15:51 -0500

    Made patch for cyurs multidomain mailbox creation optionnal.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,24 @@
 class gosa (
   $ldap_bind_dn,
   $ldap_bind_pw,
+  $patch_for_cyrus_multidomain = false,
 ) {
-  
+
   include apache2::variables
-  
+
   package { [ 'gosa', 'gosa-help-en', 'gosa-help-fr', 'gosa-plugin-dhcp', 'gosa-plugin-dns', 'gosa-plugin-log', 'gosa-plugin-mail', 'gosa-plugin-pureftpd', 'gosa-plugin-rsyslog', 'gosa-plugin-samba' ]:
     ensure  => present
   }
 
   file { '/etc/gosa/gosa.conf':
     ensure  => file,
-    mode    => 0600,
+    mode    => '0600',
     owner   => $apache2::variables::apache_user,
     group   => $apache2::variables::apache_group,
     content => template('gosa/gosa.conf.erb'),
     notify  => Exec['reload-apache2'],
-    require => Package['apache2','gosa']
+    require => Package['apache2','gosa'],
+    replace => false,
   }
 
   file { '/etc/gosa/gosa-apache.conf':
@@ -25,18 +27,18 @@ class gosa (
     notify  => Exec['reload-apache2'],
     require => Package['gosa']
   }
-  
+
   file { '/etc/gosa/shells':
     ensure  => file,
     source  => 'puppet:///modules/gosa/shells',
     notify  => Exec['reload-apache2']
   }
 
-  #Might be better to provide a new package
-  file { '/usr/share/gosa/plugins/personal/mail/class_mail-methods-cyrus.inc':
-    ensure  => file,
-    source  => 'puppet:///modules/gosa/class_mail-methods-cyrus.inc',
-    require => Package['gosa']
+  if patch_for_cyrus_multidomain { 
+    file { '/usr/share/gosa/plugins/personal/mail/class_mail-methods-cyrus.inc':
+      ensure  => file,
+      source  => 'puppet:///modules/gosa/class_mail-methods-cyrus.inc',
+      require => Package['gosa']
+    }
   }
 }
-
